5704a4171f72b99b390cebd45951cf13d75ccaea
cpc6128-fr <61292569+cpc6128-fr@users.noreply.github.com>
2021-05-09 12:03:37 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,15 +1,4 @@
-commerce={}
+subgame_spacengine={}
 
-local mp_c=minetest.get_modpath("commerce")
-dofile(mp_c .. "/data.lua")
-dofile(mp_c .. "/commerce3_2.lua")
-dofile(mp_c .. "/bank.lua")
-dofile(mp_c .. "/coffre.lua")
-dofile(mp_c .. "/licence.lua")
-dofile(mp_c .. "/civilisation.lua")
-
-minetest.register_privilege("destruct_commerce", {
-	description = "player can destruct commerce node",
-	give_to_singleplayer= true,
-})
+--init repositories
 --_____________________________________________________
